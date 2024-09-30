ae45fac15d535aa5d1544632d8e277d7b3a4e3d2
pminsun <pminsun309@gmail.com>
2024-09-30 10:47:39 +0000

    Fix : 모바일 뷰 네이버지도 이동 해결

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,16 +2,23 @@ import { ReactNode, useEffect, useState } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   useEffect(() => {
-    const disableScroll = () => {
-      document.body?.addEventListener('touchmove', removeEvent, { passive: false });
-    };
+    const disableScroll = (e: TouchEvent) => {
+      const mapElement = document.getElementById('map');
 
-    const removeEvent = (e: TouchEvent) => {
-      e.preventDefault();
+      // 터치 이벤트가 지도(map)에서 발생하는 경우는 기본 동작 허용
+      if (mapElement && mapElement.contains(e.target as Node)) {
+        return;
+      }
+
+      e.preventDefault(); // 지도 외의 터치 이벤트는 기본 동작 막기
       e.stopPropagation();
     };
 
-    disableScroll();
+    document.body?.addEventListener('touchmove', disableScroll, { passive: false });
+
+    return () => {
+      document.body?.removeEventListener('touchmove', disableScroll);
+    };
   }, []);
 
   return <section className="mobile_layout realmobile_screen">{children}</section>;

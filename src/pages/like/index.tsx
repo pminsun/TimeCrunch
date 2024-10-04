f0ac4e39d3fb5d409616944b6c45d7d98f7d30dd
pminsun <pminsun309@gmail.com>
2024-10-04 06:41:39 +0000

    Test : 로그인 테스트2

--- src/pages/like/index.tsx
+++ src/pages/like/index.tsx
@@ -1,3 +1,44 @@
+import { kkk } from '@/api/fetchData';
+import * as LocalImages from '@/utils/imageImports';
+import Image from 'next/image';
+
 export default function Like() {
-  return <div>Like</div>;
+  const loginHandler = async () => {
+    // router.push('/signup');
+    try {
+      const res = await kkk();
+      console.log(res);
+      // router.push('/signup');
+    } catch (e: any) {
+      console.error('Error Login:', e); // 에러 메시지 수정
+    }
+  };
+
+  return (
+    <div className="likepage_container">
+      <p>
+        <Image
+          src={LocalImages.markerLike}
+          alt="markerLike"
+          width={30}
+          height={30}
+        />
+        LIKE
+      </p>
+      <div className="content">
+        <div className="noLogin">
+          <p className="ment">
+            LIKE, MY PAGE는
+            <br /> 로그인 후 이용이 가능해요
+          </p>
+          <p
+            className="kakao_login"
+            onClick={loginHandler}
+          >
+            카카오 로그인
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }

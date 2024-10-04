b35c251e9060cd002f7399006858ffd18bd1744f
pminsun <pminsun309@gmail.com>
2024-10-04 10:57:39 +0000

    Test : 로그인 테스트5

--- src/pages/like/index.tsx
+++ src/pages/like/index.tsx
@@ -5,21 +5,30 @@ import { useRouter } from 'next/router';
 
 export default function Like() {
   const router = useRouter();
-  // const loginHandler = async () => {
-  //   router.push('https://api.seongsu-snack.site/oauth2/authorization/kakao');
+  const loginHandler = async () => {
+    router.push('https://api.seongsu-snack.site/oauth2/authorization/kakao');
+  };
+
+  // const getCookie = (name: string) => {
+  //   const value = `; ${document.cookie}`;
+  //   const parts = value.split(`; ${name}=`);
+  //   if (parts.length === 2) return parts.pop()?.split(';').shift();
   // };
 
-  const loginHandler = async () => {
-    // router.push('/signup');
-    try {
-      const res = await kkk();
-      console.log(res);
-      // router.push('/signup');
-      router.push('/signup');
-    } catch (e: any) {
-      console.error('Error Login:', e); // 에러 메시지 수정
-    }
-  };
+  // const loginHandler = async () => {
+  //   // router.push('/signup');
+  //   try {
+  //     const res = await kkk();
+  //     console.log(res);
+  //     // router.push('/signup');
+
+  //     // 쿠키에서 accessToken 가져오기
+  //     const accessToken = getCookie('accessToken');
+  //     console.log('Access Token:', accessToken);
+  //   } catch (e: any) {
+  //     console.error('Error Login:', e); // 에러 메시지 수정
+  //   }
+  // };
 
   return (
     <div className="likepage_container">

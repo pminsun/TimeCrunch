fdf61fa58d89a4beae74860caf9fe39c3bb2bcf7
pminsun <pminsun309@gmail.com>
2024-10-05 11:29:48 +0000

    Fix : 무드 선택 초기 지도 경계선 100m

--- src/components/MoodSelect.tsx
+++ src/components/MoodSelect.tsx
@@ -81,79 +81,8 @@ export default function MoodSelect() {
     }
   };
 
-  // useEffect(() => {
-  //   const initMap = (lat: number, lng: number, isInSeongsu: boolean) => {
-  //     const currentLocation = new naver.maps.LatLng(lat, lng);
-  //     // walkTime에 따른 줌 레벨 설정
-  //     const zoomLevel = walkTime === 0 ? 17 : walkTime === 5 ? 15 : walkTime === 10 ? 14 : walkTime === 15 ? 13 : walkTime === 20 ? 13 : 11;
-  //     const mapOptions = {
-  //       center: currentLocation,
-  //       zoom: zoomLevel,
-  //     };
-
-  //     const map = new naver.maps.Map('tempap', mapOptions);
-
-  //     // 반경 서클 생성
-  //     // walkTime에 따른 반경 값 설정 (400m, 800m, 1.2km, 1.6km, 2.4km)
-  //     const radius = walkTime === 30 ? 2400 : walkTime === 5 ? 400 : walkTime === 10 ? 800 : walkTime === 15 ? 1200 : walkTime === 20 ? 1600 : 100;
-  //     const circle = new naver.maps.Circle({
-  //       map: map,
-  //       center: currentLocation,
-  //       radius: radius,
-  //       strokeColor: '#FF977A',
-  //       strokeOpacity: 1,
-  //       strokeWeight: 1,
-  //       fillColor: '#FEAD97',
-  //       fillOpacity: 0.11,
-  //     });
-  //   };
-
-  //   const loadMap = () => {
-  //     // 성수동 중심 좌표
-  //     const defaultLat = 37.544579;
-  //     const defaultLng = 127.055831;
-
-  //     // 성공 콜백 함수
-  //     const success = (position: GeolocationPosition) => {
-  //       const { latitude, longitude } = position.coords;
-
-  //       // 성수동의 대략적인 범위 설정
-  //       const isInSeongsu = latitude >= 37.543 && latitude <= 37.546 && longitude >= 127.054 && longitude <= 127.058;
-
-  //       if (isInSeongsu) {
-  //         initMap(latitude, longitude, isInSeongsu); // 성수동 내 위치
-  //       } else {
-  //         initMap(defaultLat, defaultLng, isInSeongsu); // 성수동 외 위치
-  //       }
-  //     };
-
-  //     // 오류 콜백 함수
-  //     const error = () => {
-  //       initMap(defaultLat, defaultLng, false); // 오류가 발생하면 성수역 좌표로 지도 초기화
-  //     };
-
-  //     // 현재 위치 확인
-  //     if (navigator.geolocation) {
-  //       navigator.geolocation.watchPosition(success, error, {
-  //         enableHighAccuracy: true, // 높은 정확도 사용
-  //       }); // 현재 위치 요청
-  //     } else {
-  //       // Geolocation을 지원하지 않으면 기본 좌표 사용
-  //       initMap(defaultLat, defaultLng, false);
-  //     }
-  //   };
-
-  //   if (window.naver && window.naver.maps) {
-  //     loadMap();
-  //   } else {
-  //     const mapScript = document.createElement('script');
-  //     mapScript.onload = () => loadMap();
-  //     mapScript.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_ID}&submodules=geocoder`;
-  //     document.head.appendChild(mapScript);
-  //   }
-  // }, [walkTime]);
-
   const circleRef = useRef<naver.maps.Circle | null>(null);
+  const mapRef = useRef<naver.maps.Map | null>(null);
 
   useEffect(() => {
     const initMap = (lat: number, lng: number, isInSeongsu: boolean) => {
@@ -175,7 +104,7 @@ export default function MoodSelect() {
         circleRef.current = new naver.maps.Circle({
           map: map,
           center: currentLocation,
-          radius: radius,
+          radius: currentSlide === 1 ? 100 : radius,
           strokeColor: '#FF977A',
           strokeOpacity: 1,
           strokeWeight: 1,
@@ -232,7 +161,7 @@ export default function MoodSelect() {
       mapScript.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_ID}&submodules=geocoder`;
       document.head.appendChild(mapScript);
     }
-  }, [walkTime]);
+  }, [walkTime, currentSlide]);
 
   return (
     <>
